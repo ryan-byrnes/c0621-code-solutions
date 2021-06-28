7856db291bbf211f77fc52207d274a98e98a09cb
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-06-28 16:52:37 -0700

    Completed javascript-arrays exercise.

---
+++ javascript-arrays/main.js
@@ -0,0 +1,25 @@
+var colors = ['red', 'white', 'blue'];
+console.log('index 0 of colors:', colors[0]);
+console.log('index 1 of colors:', colors[1]);
+console.log('index 2 of colors', colors[2]);
+
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
+
+colors[2] = 'green';
+
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
+console.log('value of colors:', colors);
+
+var students = ['Rex', 'Ralph', 'Ben', 'Barry'];
+
+var numberOfStudents = students.length;
+
+console.log('value of numberOfStudents:', numberOfStudents);
+
+var lastIndex = numberOfStudents - 1;
+
+var lastStudent = students[lastIndex];
+
+console.log('value of lastStudent:', lastStudent);
+
+console.log('value of students:', students);
