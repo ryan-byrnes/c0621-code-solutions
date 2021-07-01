875e67aa7b656bc06fe7b2a65bfd0a309e1a56f0
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-01 16:48:17 -0700

    removed key and added name.

--- javascript-loops-practice/get-student-names.js
+++ javascript-loops-practice/get-student-names.js
@@ -2,8 +2,8 @@
 function getStudentNames(students) {
   var names = [];
   for (var i = 0; i < students.length; i++) {
-    for (var key in students[i]) {
-      names.push(students[i][key]);
+    for (var name in students[i]) {
+      names.push(students[i][name]);
     }
   }
   return names;
