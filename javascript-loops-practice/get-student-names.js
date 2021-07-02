64fbd644856276149275ad860224b1c84f47c369
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-01 17:02:00 -0700

    Removed for in loop, edited for loop code block.

--- javascript-loops-practice/get-student-names.js
+++ javascript-loops-practice/get-student-names.js
@@ -2,9 +2,7 @@
 function getStudentNames(students) {
   var names = [];
   for (var i = 0; i < students.length; i++) {
-    for (var name in students[i]) {
-      names.push(students[i][name]);
-    }
+    names.push(students[i].name);
   }
   return names;
 }
