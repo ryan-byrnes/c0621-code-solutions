34eed81707b91f0627ca39423b16757f069e6551
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-06 11:13:25 -0700

    working on getWords.

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -4,12 +4,17 @@ iterate through string looking for a space character that indicates the end of a
 push word into an array
 push final characters into array (final word of the array) */
 
-function getWords(string) {
+/* function getWords(string) {
   var wordsArray = [];
   for (var i = 0; i < string.length; i++) {
     if (string[i] === ' ' || string.length > 1) {
       wordsArray = string.split(' ');
     }
   }
+  return wordsArray;
+} */
+
+function getWords(string) {
+  var wordsArray = string.split(' ');
   return wordsArray;
 }
