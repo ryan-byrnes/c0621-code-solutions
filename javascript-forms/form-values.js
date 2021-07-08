7ab7740820c02b9c1ec74aa6aed55bec5465a6ab
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-08 12:01:22 -0700

    Removed for loop, and used the element named properties to add the input values to an object.

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -4,15 +4,9 @@ var values = {};
 
 contactForm.addEventListener('submit', function () {
   event.preventDefault();
-  for (var i = 0; i < inputs.length; i++) {
-    if (inputs[i].name === 'name') {
-      values.name = inputs[i].value;
-    } else if (inputs[i].name === 'email') {
-      values.email = inputs[i].value;
-    } else if (inputs[i].name === 'message') {
-      values.message = inputs[i].value;
-      console.log(values);
-      contactForm.reset();
-    }
-  }
+  values.name = inputs.name.value;
+  values.email = inputs.email.value;
+  values.message = inputs.message.value;
+  console.log(values);
+  contactForm.reset();
 });
