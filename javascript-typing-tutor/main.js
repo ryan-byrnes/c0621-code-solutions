1699d888000b456cae78fc49ccfb06c5447dde53
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-08 12:42:18 -0700

    Completed javascript-typing-tutor challenge

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -5,6 +5,7 @@ document.addEventListener('keydown', function keyPress(e) {
   if (character[letter].textContent === e.key) {
     character[letter].className = 'correct-character';
     letter++;
+    character[letter].className = 'current-character';
   } else {
     character[letter].className = 'current-character incorrect-character';
   }
