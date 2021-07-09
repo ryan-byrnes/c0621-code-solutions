8ecc7741dee9c7f1f9ae47800a881ae1a4121e3c
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-09 16:57:18 -0700

    Completed javascript-view-swapping

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,5 +1,6 @@
 var tabContainer = document.querySelector('.tab-container');
 var tabElements = document.querySelectorAll('.tab');
+var viewElements = document.querySelectorAll('.view');
 
 tabContainer.addEventListener('click', matchesEvent);
 
@@ -10,6 +11,14 @@ function matchesEvent() {
         tabElements[i].className = 'tab active';
       } else {
         tabElements[i].className = 'tab';
+      }
+    }
+    var dataView = event.target.getAttribute('data-view');
+    for (i = 0; i < viewElements.length; i++) {
+      if (dataView === viewElements[i].getAttribute('data-view')) {
+        viewElements[i].className = 'view';
+      } else {
+        viewElements[i].className = 'view hidden';
       }
     }
   }
