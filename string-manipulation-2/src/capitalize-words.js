6b8988102e689a04e28d812bae2996faed249760
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-12 11:16:15 -0700

    working on capitalizeWords git status

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,19 @@
 /* exported capitalizeWords */
+/* split the string at any spaces
+loop through the return array
+set word to lower case
+upper case the first letter and assign to a new variable array
+join new variable array */
+
+function capitalizeWords(string) {
+  var splitString = [];
+  var capitalizeString = [];
+  splitString = string.split(' ');
+  for (var i = 0; i < splitString.length; i++) {
+    splitString[i].toLowerCase();
+    splitString[i].charAt(0).toUpperCase();
+    capitalizeString.push(splitString[i]);
+  }
+  return capitalizeString.join(' ');
+
+}
