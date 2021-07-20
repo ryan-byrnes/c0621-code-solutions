7f6bcca02df39f4bc7efcd2d9faac07df1d2a5dd
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-20 15:09:14 -0700

    finished firstChars

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,11 @@
 /* exported firstChars */
+/* string.slice(length) */
+
+function firstChars(length, string) {
+  var newString = [];
+  var spliceString = string.split('');
+  for (var i = 0; i < length; i++) {
+    newString.push(spliceString[i]);
+  }
+  return newString.join('');
+}
