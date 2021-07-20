ef26c44d0b3c19e939baa540627b754d27725710
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-20 14:59:30 -0700

    finished num-vowels

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,14 @@
 /* exported numVowels */
+/* iterate through string
+if character[i] is a vowel, add 1 to a counter variable
+return counter */
+
+function numVowels(string) {
+  var vowelCount = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string[i].toLowerCase() === 'a' || string[i].toLowerCase() === 'e' || string[i].toLowerCase() === 'i' || string[i].toLowerCase() === 'o' || string[i].toLowerCase() === 'u') {
+      vowelCount += 1;
+    }
+  }
+  return vowelCount;
+}
