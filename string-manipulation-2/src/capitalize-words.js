b0fca26094c8e6337def1af4851d60e3a73a5afd
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-20 09:56:40 -0700

    working on capitalize words

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -7,12 +7,13 @@ join new variable array */
 
 function capitalizeWords(string) {
   var splitString = [];
+  var newString = [];
   var capitalizeString = [];
   splitString = string.split(' ');
   for (var i = 0; i < splitString.length; i++) {
     splitString[i].toLowerCase();
-    splitString[i].charAt(0).toUpperCase();
-    capitalizeString.push(splitString[i]);
+    newString = splitString[i].charAt(0).toUpperCase() + splitString[i].slice(1);
+    capitalizeString.push(newString);
   }
   return capitalizeString.join(' ');
 
