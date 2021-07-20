594bdf6ba34efa37c3192ba8f5bc0c53592e8c65
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-20 11:48:59 -0700

    Removed else if statement and replaced with else statements in account.js.

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -10,7 +10,7 @@ Account.prototype.deposit = function (amount) {
   if (amount > 0 && Number.isInteger(amount)) {
     this.transactions.push(new Transaction('deposit', amount));
     return true;
-  } else if (amount <= 0 || isNaN(amount) || !Number.isInteger(amount)) {
+  } else {
     return false;
   }
 };
@@ -19,7 +19,7 @@ Account.prototype.withdraw = function (amount) {
   if (amount > 0 && Number.isInteger(amount)) {
     this.transactions.push(new Transaction('withdrawal', amount));
     return true;
-  } else if (amount <= 0 || isNaN(amount) || !Number.isInteger(amount)) {
+  } else {
     return false;
   }
 };
