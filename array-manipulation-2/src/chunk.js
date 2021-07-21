49cc2261f6f646fa771b89c679746c08400d0d1b
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-20 17:30:11 -0700

    Completed array-manipulation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,8 @@
 /* exported chunk */
+function chunk(array, size) {
+  var chunkArray = [];
+  for (var i = 0; i < array.length; i += size) {
+    chunkArray.push(array.slice(i, i + size));
+  }
+  return chunkArray;
+}
