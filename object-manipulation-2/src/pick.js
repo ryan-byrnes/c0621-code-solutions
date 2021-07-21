a534b9a58ca033b5ce7317b77af4fe1794afee4b
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-20 17:53:46 -0700

    Just started object-manipulation-2

---
+++ object-manipulation-2/src/pick.js
@@ -0,0 +1,8 @@
+/* exported pick */
+/* loop through object properties
+if property name matches array key value name, add that property and key to a new object
+return new object */
+
+function pick(source, keys) {
+
+}
