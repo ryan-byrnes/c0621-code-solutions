a34c0416a74b2ea5b881382931c355e16423c19e
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-08-03 20:54:15 -0700

    Completed invert() function

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -3,3 +3,12 @@
 key[i].value = key[i]
 assign the new key value pairs to a new object
 return new object */
+
+function invert(source) {
+  var newObj = {};
+
+  for (var key in source) {
+    newObj[source[key]] = key;
+  }
+  return newObj;
+}
