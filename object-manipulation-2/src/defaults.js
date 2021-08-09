8316f459a9432bce08dcc507934daf879a874ce3
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-08-09 12:55:55 -0700

    Completed object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,12 @@
 /* exported defaults */
+/* check to see if key exists in target
+if it does not, add property to target */
+
+function defaults(target, source) {
+  for (const key in source) {
+    if (target[key] === undefined) {
+      target[key] = source[key];
+    }
+  }
+  return target;
+}
