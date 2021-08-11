81f29eee64b879d98f57e715635d115a54907d46
ryan-byrnes <ryn_byrnes@yahoo.com>
2021-08-11 00:02:11 +0000

    Commit defaults.js

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,6 +1,7 @@
 /* exported defaults */
 /* check to see if key exists in target
-if it does not, add property to target */
+if it does not, add property to target
+*/
 
 function defaults(target, source) {
   for (const key in source) {
