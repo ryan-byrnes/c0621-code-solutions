e94d5282b664baf6ebeded46b5a96980790cf9c0
ryan-byrnes <ryn_byrnes@yahoo.com>
2021-08-14 00:00:49 +0000

    Completed express-get-json

---
+++ express-get-json/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+const app = express();
+
+app.get('/api/grades', function (req, res) {
+  const gradesArray = [];
+  for (const key in grades) {
+    gradesArray.push(grades[key]);
+  }
+  res.json(gradesArray);
+});
+
+app.listen(3000, () => {
+
+});
