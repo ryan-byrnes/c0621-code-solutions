3a88975fce4a19876d1a3ce9a2aea31d1332998d
ryan-byrnes <ryn_byrnes@yahoo.com>
2021-08-17 14:56:19 +0000

    express-notes-api

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -17,58 +17,58 @@ app.get('/api/notes', (req, res) => {
 
 app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id;
-  if (id < 1) {
-    res.sendStatus(400).send('Id must be a positive number');
+  if (id < 1 || isNaN(id)) {
+    res.status(400).send('Id must be a positive number');
   } else if (data.notes[id]) {
     res.json(data.notes[id]);
   } else {
-    res.sendStatus(404).send(`Error: Could not find note with id number: ${id}`);
+    res.status(404).send(`Error: Could not find note with id number: ${id}`);
   }
 });
 
 app.post('/api/notes', (req, res) => {
   if (!req.body.content) {
-    res.sendStatus(400).send('Error: Please enter note content.');
+    res.status(400).send('Error: Please enter note content.');
   } else if (req.body.content) {
     req.body.id = data.nextId;
     data.notes[data.nextId] = req.body;
     data.nextId++;
     updateData();
-    res.sendStatus(201).send(data.notes[data.nextId]);
+    res.status(201).send(req.body);
 
   } else {
-    res.sendStatus(500).send('Error: An unexpected error occurred.');
+    res.status(500).send('Error: An unexpected error occurred.');
   }
 });
 
 app.delete('/api/notes/:id', (req, res) => {
   const id = req.params.id;
   if (id < 1 || isNaN(id)) {
-    res.sendStatus(400).send('Please enter a valid Id number');
+    res.status(400).send('Please enter a valid Id number');
   } else if (!id) {
-    res.sendStatus(404).send('Id number does not exist');
+    res.status(404).send('Id number does not exist');
   } else if (id) {
     delete data.notes[id];
     updateData();
     res.sendStatus(204);
   } else {
-    res.sendStatus(500).send('Error: An unexpected error occurred');
+    res.status(500).send('Error: An unexpected error occurred');
   }
 });
 
 app.put('/api/notes/:id', (req, res) => {
   const id = req.params.id;
   if (id < 1 || isNaN(id)) {
-    res.sendStatus(400).send('Please enter a valid Id number');
+    res.status(400).send('Please enter a valid Id number');
   } else if (!id) {
-    res.sendStatus(404).send('Id number does not exist');
+    res.status(404).send('Id number does not exist');
   } else if (id) {
-    req.body.id = id;
+    req.body.id = parseInt(id);
     data.notes[id] = req.body;
     updateData();
-    res.sendStatus(200).send(data.notes[id]);
+    res.json(data.notes[id]);
   } else {
-    res.sendStatus(500).send('Error: An unexpected error occurred');
+    res.status(500).send('Error: An unexpected error occurred');
   }
 });
 
