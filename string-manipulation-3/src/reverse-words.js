02d9ee2e82d371d2218555b9f7602d76ffe178b7
ryan-byrnes <ryn_byrnes@yahoo.com>
2021-08-20 19:28:55 +0000

    working on reverseWords

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,14 +1,1 @@
 /* exported reverseWords */
-
-const reverseWords = string => {
-  const splitString = string.split(' ');
-  const reverseWordsArray = [];
-  let reverseWordString = '';
-
-  for (let i = 0; i < splitString.length; i++) {
-    reverseWordString = splitString[i].reverse();
-    reverseWordsArray.push(reverseWordString);
-  }
-  return reverseWordsArray.join();
-
-};
