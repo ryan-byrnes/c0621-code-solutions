028fc74483b0802b48584d81747d345655d89fce
ryan-byrnes <ryn_byrnes@yahoo.com>
2021-08-23 19:38:46 +0000

    Finished reverseWords

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,15 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  let reverseString = '';
+  let reverseWord = '';
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== ' ') {
+      reverseWord = string[i] + reverseWord;
+    } else {
+      reverseString += `${reverseWord} `;
+      reverseWord = '';
+    }
+  }
+  reverseString += reverseWord;
+  return reverseString;
+}
