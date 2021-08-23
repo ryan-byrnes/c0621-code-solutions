604874b7862ccd84a3824a39626a2c75eba0da5c
ryan-byrnes <ryn_byrnes@yahoo.com>
2021-08-23 19:46:07 +0000

    Started titleCase

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,11 @@
 /* exported titleCase */
+/*
+iterate through string and capitalize the first letter of each word (first letter and first letter following a space)
+slice each character we want, store it in a new var, and call toUpperCase
+Add the letter back to the string
+return new titlecase string
+*/
+
+function titleCase(title) {
+
+}
