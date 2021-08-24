331ff8c56eebad052992ae94e6c90d6c130cbc5c
ryan-byrnes <ryn_byrnes@yahoo.com>
2021-08-24 18:40:21 +0000

    Completed react-function-components

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function CustomButton(props) {
+  return <button>Click me</button>;
+}
+
+ReactDOM.render(
+  CustomButton(),
+  document.querySelector('#root')
+);
