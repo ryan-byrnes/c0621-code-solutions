29db27188dd2850eae0484ec8bd3b4bde2dae2ea
ryan-byrnes <ryn_byrnes@yahoo.com>
2021-08-27 00:58:47 +0000

    need to hide password, and edit styling

---
+++ react-validated-input-component/src/index.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class PasswordInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isValid: false,
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
+  render() {
+    if (this.state.value.length < 8 && this.state.value.length > 0) {
+      return (
+        <form>
+          <label>
+            <p>Password</p>
+            <input type="text" onChange={this.handleChange}></input>
+            <i className="padding-left color-red fas fa-times"></i>
+          </label>
+          <p className="color-red">Your password is too short.</p>
+        </form>
+      );
+    } else if (this.state.value.length === 0) {
+      return (
+        <form>
+          <label>
+            <p>Password</p>
+            <input type="text" onChange={this.handleChange}></input>
+            <i className="padding-left color-red fas fa-times"></i>
+          </label>
+          <p className="color-red">A password is required.</p>
+        </form>
+      );
+    } else {
+      return (
+        <form>
+          <label>
+            <p>Password</p>
+            <input type="text" onChange={this.handleChange}></input>
+            <i className="padding-left color-green fas fa-check"></i>
+          </label>
+        </form>
+      );
+    }
+  }
+}
+
+ReactDOM.render(
+  <PasswordInput />,
+  document.querySelector('#root')
+);
