095269179bc52fc2a184aad5066d06181a998efe
ryan-byrnes <ryn_byrnes@yahoo.com>
2021-08-27 00:03:03 +0000

    completed react-app-drawer-component

--- react-app-drawer-component/src/index.jsx
+++ react-app-drawer-component/src/index.jsx
@@ -20,6 +20,34 @@ class Drawer extends React.Component {
   }
 
   render() {
-
+    if (!this.state.isOpen) {
+      return (
+      <i className="fas fa-bars" onClick={this.handleOpen}></i>
+      );
+    } else {
+      return (
+        <div className="background" onClick={this.handleClose}>
+          <div className="menu-container" onClick={this.handleClose}>
+            <div>
+              <h1>Menu Items</h1>
+            </div>
+            <div>
+              <ul>
+                <li>Item</li>
+                <li>Item</li>
+                <li>Item</li>
+                <li>Item</li>
+                <li>Item</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
+
+ReactDOM.render(
+  <Drawer />,
+  document.querySelector('#root')
+);
