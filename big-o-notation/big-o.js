f86ae96d16d94fdffd414ced0b974b6bbc215ef1
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-09-21 15:24:14 -0700

    Corrected errors

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -5,13 +5,13 @@ function uniqueLinear(words) {
   const unique = [];                // 1 * 1 = O(1)
   for (
     let i = 0;                      // 1 * 1 = O(1)
-    i < words.length;               // 2 * n = O(2n)
-    i++                             // 2 * n = O(2n)
+    i < words.length;               // 2 * n = O(n)
+    i++                             // 2 * n = O(n)
   ) {
-    const word = words[i];          // 2 * n = O(2n)
-    if (!seen[word]) {              // 2 * n = O(2n)
-      seen[word] = true;
-      unique[unique.length] = word; // 3 * n = O(3n)
+    const word = words[i];          // 2 * n = O(n)
+    if (!seen[word]) {              // 3 * n = O(n)
+      seen[word] = true;            // 2 * n = 0(n)
+      unique[unique.length] = word; // 3 * n = O(n)
     }
   }
   return unique;
@@ -21,23 +21,23 @@ function uniqueQuadratic(words) {
   const unique = [];                // 1 * 1 = O(1)
   for (
     let i = 0;                      // 1 * 1 = O(1)
-    i < words.length;               // 2 * n = O(2n)
-    i++                             // 2 * n = O(2n)
+    i < words.length;               // 2 * n = O(n)
+    i++                             // 2 * n = O(n)
   ) {
-    const word = words[i];          // 2 * n = O(2n)
+    const word = words[i];          // 2 * n = O(n)
     let isUnique = true;            // 1 * n = O(n)
     for (
-      let c = 0;                    // 1 * n^2 = O(n^2)
+      let c = 0;                    // 1 * n = O(n)
       c < i;                        // 1 * n^2 = O(n^2)
-      c++                           // 2 * n^2 = O(2n^2)
+      c++                           // 2 * n^2 = O(n^2)
     ) {
-      const comparing = words[c];   // 2 * n^2 = O(2n^2)
-      if (comparing === word) {     // 1 * n^2 = O(n^2)
+      const comparing = words[c];   // 2 * n^2 = O(n^2)
+      if (comparing === word) {     // 2 * n^2 = O(n^2)
         isUnique = false;           // 1 * n^2 = O(n^2)
       }
     }
     if (isUnique) {                 // 1 * n = O(n)
-      unique[unique.length] = word; // 3 * n = O(3n)
+      unique[unique.length] = word; // 3 * n = O(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
