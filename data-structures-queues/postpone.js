068c026c04ab1f0a81e78db9782ca49ade46e3dc
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-09-22 11:32:42 -0700

    Refactored take2nd, and removed arguments from dequeue method calls in other functions

--- data-structures-queues/postpone.js
+++ data-structures-queues/postpone.js
@@ -3,7 +3,7 @@
 function postpone(queue) {
   const front = queue.peek();
   if (front !== undefined) {
-    const taken = queue.dequeue(front);
+    const taken = queue.dequeue();
     return queue.enqueue(taken);
   }
 }
--- data-structures-queues/take-2nd.js
+++ data-structures-queues/take-2nd.js
@@ -1,15 +1,11 @@
 /* exported take2nd */
 
 function take2nd(queue) {
-  const front = queue.peek();
-  if (front !== undefined) {
-    const taken = queue.dequeue(front);
-    if (queue.peek() !== undefined) {
-      const second = queue.dequeue();
-      queue.enqueue(taken);
-      return second;
-    } else {
-      return taken;
-    }
+  const front = queue.dequeue();
+  if (queue.peek() !== undefined) {
+    const second = queue.dequeue();
+    queue.enqueue(front);
+    return second;
   }
+  return front;
 }
