797b21a613c9df65a320cf63fd5b882b9bb83289
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-10-16 14:00:05 -0700

    completed difference

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,15 @@
 /* exported difference */
+function difference(first, second) {
+  const newArray = [];
+  for (let i = 0; i < first.length; i++) {
+    if (second.indexOf(first[i]) === -1) {
+      newArray.push(first[i]);
+    }
+  }
+  for (let i = 0; i < second.length; i++) {
+    if (first.indexOf(second[i]) === -1) {
+      newArray.push(second[i]);
+    }
+  }
+  return newArray;
+}
