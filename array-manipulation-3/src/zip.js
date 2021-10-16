28560087081d7d45c69205e4aa79eaf5f4dc2f0d
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-10-16 13:48:35 -0700

    completed zip

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,12 @@
 /* exported zip */
+function zip(first, second) {
+  let input = first;
+  const newArray = [];
+  if (first.length > second.length) {
+    input = second;
+  }
+  for (let i = 0; i < input.length; i++) {
+    newArray.push([first[i], second[i]]);
+  }
+  return newArray;
+}
