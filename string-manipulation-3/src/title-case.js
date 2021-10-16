696bff09845ff08da13a5591b8a804d6bdaa72ff
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-10-16 14:33:57 -0700

    completed string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,21 +1,32 @@
 /* exported titleCase */
-/*
-iterate through string and capitalize the first letter of each word (first letter and first letter following a space)
-slice each character we want, store it in a new var, and call toUpperCase
-Add the letter back to the string
-return new titlecase string
-*/
 
-function titleCase(title) {
-  const minor = ['for', 'and', 'or', 'nor', 'but', 'as', 'by', 'for', 'in'];
-  const words = title.split(' ');
-  const newString = '';
-
-  for (let i = 0; i < words.length; i++) {
-    let word = words[i].toLowerCase();
-
-    if (word === 'javascript') {
-      word = 'JavaScript';
+function titleCase(string) {
+  const minorWords = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
+  let newString = string.split(' ');
+  for (let i = 0; i < newString.length; i++) {
+    if (newString[i] === 'api') {
+      newString[i] = newString[i].toUpperCase();
+    }
+    if (newString[i].toLowerCase().includes('javascript')) {
+      if (newString[i].includes(':')) {
+        newString[i] = 'JavaScript:';
+      } else {
+        newString[i] = 'JavaScript';
+      }
+    }
+    if (newString[i] === newString[0] || !minorWords.includes(newString[i])) {
+      newString[i] = newString[i][0].toUpperCase() + newString[i].substring(1);
+    }
+    if (newString[i].includes(':')) {
+      newString[i + 1] = newString[i + 1][0].toUpperCase() + newString[i + 1].substring(1);
+    }
+    if (newString[i].includes('-')) {
+      let dashString = newString[i].split('-');
+      dashString[1] = dashString[1][0].toUpperCase() + dashString[1].substring(1);
+      dashString = dashString.join('-');
+      newString[i] = dashString;
     }
   }
+  newString = newString.join(' ');
+  return newString;
 }
