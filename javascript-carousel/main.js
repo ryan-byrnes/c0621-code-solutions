46bbbbdabc9b004a147b7c3a26fd32c27e644489
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-10-18 14:59:59 -0700

    Reworked most of main.js

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,58 +1,58 @@
-var counter = 1;
-var intervalId = setInterval(switchImageTimer, 3000);
-switchImageTimer();
-
-function switchImageTimer() {
-  var image = document.querySelectorAll('img');
-  var circle = document.querySelectorAll('.fa-circle');
-  if (counter > 5) {
-    counter = 1;
-  }
-  if (counter === 0) {
-    counter = 5;
-  }
-  if (counter <= 5 && counter > 0) {
-    for (var i = 0; i < image.length; i++) {
-      if (counter === parseInt(image[i].getAttribute('image-view'))) {
-        image[i].setAttribute('class', '');
-        circle[i].setAttribute('class', 'fas fa-circle padding-left-right');
-      } else {
-        image[i].setAttribute('class', 'hidden');
-        circle[i].setAttribute('class', 'far fa-circle padding-left-right');
-      }
-    }
-  }
-  counter += 1;
-}
+var counter = 0;
+var image = document.querySelectorAll('img');
+var circle = document.querySelectorAll('.fa-circle');
+var circleClicked = document.querySelector('.circle-clicked');
+var chevronLeft = document.querySelector('.fa-chevron-left');
+var chevronRight = document.querySelector('.fa-chevron-right');
+var intervalId = setInterval(display, 3000);
 
 function previousOrNextImage(event) {
-  var chevronLeft = document.querySelector('.fa-chevron-left');
-  var chevronRight = document.querySelector('.fa-chevron-right');
-
+  clearInterval(intervalId);
+  intervalId = setInterval(display, 3000);
   if (event.target === chevronLeft) {
-    clearInterval(intervalId);
-    counter -= 2;
-    switchImageTimer();
-    intervalId = setInterval(switchImageTimer, 3000);
-  } else if (event.target === chevronRight) {
-    clearInterval(intervalId);
-    switchImageTimer();
-    intervalId = setInterval(switchImageTimer, 3000);
+    counter--;
+  }
+  if (event.target === chevronRight) {
+    counter++;
+  }
+  if (counter === image.length) {
+    counter = 0;
+  }
+  if (counter === -1) {
+    counter = image.length - 1;
+  }
+  next();
+}
+
+function next() {
+  for (let i = 0; i < image.length; i++) {
+    if (i === counter) {
+      image[i].setAttribute('class', '');
+      circle[i].setAttribute('class', 'fas fa-circle padding-left-right');
+    } else {
+      image[i].setAttribute('class', 'hidden');
+      circle[i].setAttribute('class', 'far fa-circle padding-left-right');
+    }
   }
 }
 
-document.addEventListener('click', previousOrNextImage);
-document.addEventListener('click', circleClick);
+function display() {
+  counter++;
+  if (counter === image.length) {
+    counter = 0;
+  }
+  next();
+}
+
+chevronLeft.addEventListener('click', previousOrNextImage);
+chevronRight.addEventListener('click', previousOrNextImage);
+circleClicked.addEventListener('click', circleClick);
 
 function circleClick(event) {
-  var circle = document.querySelectorAll('.fa-circle');
-  for (var i = 0; i < circle.length; i++) {
-    if (event.target === circle[i]) {
-      clearInterval(intervalId);
-      counter = parseInt(event.target.getAttribute('circle-view'));
-      switchImageTimer();
-      event.target.setAttribute('class', 'fas fa-circle padding-left-right');
-      intervalId = setInterval(switchImageTimer, 3000);
-    }
+  clearInterval(intervalId);
+  intervalId = setInterval(display, 3000);
+  if (event.target.matches('.far')) {
+    counter = parseInt(event.target.getAttribute('circle-view'));
+    next();
   }
 }
